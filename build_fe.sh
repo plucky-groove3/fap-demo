20b57eddc620fb6beadcbe9ed629d994c89ed5e3
lucas.hsueh <lucas_hsueh@hotmail.com>
2020-11-29 16:53:58 +0800

    Revert "aaa"
    This reverts commit 8e9594a15f1931906f63798c69359c642d121cfa.

--- build_fe.sh
+++ build_fe.sh
@@ -9,7 +9,7 @@ cd ./frontend
 # ng build --prod --base-href /${PROJECT_NAME}/${ANGULAR_APP_NAME}
 # npx ngh --dir=docs/${ANGULAR_APP_NAME}
 
-ng build --prod
+ng build --prod --base-href /${PROJECT_NAME}/docs
 npx ngh --dir=../docs
 
 echo "build frontend done!"
