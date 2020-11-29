3b9ffd68bb126d8334836927e47f6b8db992901d
lucas.hsueh <lucas_hsueh@hotmail.com>
2020-11-29 16:26:25 +0800

    aa

--- build_fe.sh
+++ build_fe.sh
@@ -6,7 +6,10 @@ ANGULAR_APP_NAME="frontend"
 echo "start build frontend!"
 
 cd ./frontend
-ng build --prod --base-href /${PROJECT_NAME}/${ANGULAR_APP_NAME}
-npx ngh --dir=docs/${ANGULAR_APP_NAME}
+# ng build --prod --base-href /${PROJECT_NAME}/${ANGULAR_APP_NAME}
+# npx ngh --dir=docs/${ANGULAR_APP_NAME}
+
+ng build --prod
+npx ngh --dir=../docs
 
 echo "build frontend done!"
